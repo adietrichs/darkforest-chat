c3b8ffe1c59a25172ce1f80ca4121df97c40719f
Ansgar Dietrichs <adietrichs@gmail.com>
2021-01-06 14:49:33 +0100

    implemented dummy Whitelist contract for testing

--- contracts/DarkForest.sol
+++ contracts/DarkForest.sol
@@ -4,3 +4,19 @@ pragma solidity ^0.7.0;
 interface Whitelist {
   function isWhitelisted(address _addr) external view returns (bool);
 }
+
+contract WhitelistDummy {
+  mapping(address => bool) allowedAccounts;
+
+  function isWhitelisted(address _addr) public view returns (bool) {
+    return allowedAccounts[_addr];
+  }
+
+  function addToWhitelist(address _addr) external {
+    allowedAccounts[_addr] = true;
+  }
+
+  function removeFromWhitelist(address _addr) external {
+    allowedAccounts[_addr] = false;
+  }
+}
